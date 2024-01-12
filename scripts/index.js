d4f7d779c0286baebbe3c03118b5ff8e45803e6f
Fariddun Kadirov <90576595+kadirov-Fariddun@users.noreply.github.com>
2024-01-12 11:19:21 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ function headerFixed(){
             menuMobile.classList.add('header-fixed');
             logo.src = '/images/logo.webp';
         }else{
-            logo.src = '../images/logo-white.webp'
+            logo.src = '/images/logo-white.webp'
             menuMobile.classList.remove('header-fixed');
             menuDesctop.classList.remove('header-fixed');
         }
